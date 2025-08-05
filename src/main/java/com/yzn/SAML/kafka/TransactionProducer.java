a7ea658f5cdf0e026e48bc427b70191c69b7a499
Yazan Alqadery <yazanalqadery@gmail.com>
2025-08-05 20:25:41 +0400

    Implemented the Transaction Producer

---
+++ src/main/java/com/yzn/SAML/kafka/TransactionProducer.java
@@ -0,0 +1,27 @@
+package com.yzn.SAML.kafka;
+
+
+import com.yzn.SAML.model.Transaction;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionProducer {
+
+    private final KafkaTemplate<String, Transaction> kafkaTemplate;
+
+
+    public TransactionProducer(KafkaTemplate<String, Transaction> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void publishTransaction(Transaction transaction){
+        Message<Transaction> message = MessageBuilder
+                .withPayload(transaction)
+                .build();
+
+        kafkaTemplate.send(message);
+    }
+}
